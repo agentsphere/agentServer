b4a3afacfb7a8cc475ea16d5160d8cb51f9a1a06
luctrate <stev.hooker@gmail.com>
2025-03-18 14:01:03 +0100

    add keycloak token verification

--- Dockerfile
+++ Dockerfile
@@ -59,6 +59,6 @@ ENV PORT=8000
 EXPOSE 8000
 #RUN uvicorn
 # Command to run the application
-CMD ["/bin/sh", "-c", "uvicorn main:app --host 0.0.0.0"]
+CMD ["/bin/sh", "-c", "uvicorn main:app --host 0.0.0.0 --port $PORT"]
 # Use official Python image
 
--- main.py
+++ main.py
@@ -9,12 +9,82 @@ from pydantic import BaseModel
 import httpx
 import logging
 
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
 import uvicorn
+
+import os
+import requests
+
+from fastapi import HTTPException, Header, Depends, status
+
+
+def introspect_token(token: str) -> dict:
+    if token is None: 
+       raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token introspection failed"
+        ) 
+    introspection_url = "https://auth.agentsphere.cloud/realms/agentsphere/protocol/openid-connect/token/introspect"
+    client_id = "toolserver"
+    client_secret = os.getenv("CLIENT_SECRET")
+    response = requests.post(
+        introspection_url,
+        headers={"Content-Type":"application/x-www-form-urlencoded"},
+        data={"token": token, "client_id": client_id, "client_secret": client_secret}
+    )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token introspection failed"
+        )
+
+
+from pydantic import BaseModel, Field
+
+class User(BaseModel):
+    id: str 
+    role: str 
+    username: str
+    mail: str
+    token: str 
+
+
+def get_user_headers(
+    user_id: Optional[str] = Header(..., alias="X-OpenWebUI-User-Id"),
+    user_role: Optional[str] = Header(..., alias="X-OpenWebUI-User-Role"),
+    user_name: Optional[str] = Header(..., alias="X-OpenWebUI-User-Name"),
+    user_email: Optional[str] = Header(..., alias="X-OpenWebUI-User-Email"),
+    token: Optional[str] = Header(..., alias="Authorization")
+):
+    return {
+        "id": user_id,
+        "role": user_role,
+        "username": user_name,
+        "mail": user_email,
+        "token": token
+    }
+
+
+def get_user(user_headers: dict = Depends(get_user_headers)):
+    introspect_token(user_headers.get("token", None))
+    return User(**user_headers)
+
+def validate_token(token_header: dict = Depends(get_user_headers)):
+    introspect_token(token_header.get("token", None))
+    return
+
 
 logger=logging.getLogger(__name__)
 
 app = FastAPI()
-TOKEN="7ajauszqwmdhjg.ax9"
 class Message(BaseModel):
     role: str
     content: str
@@ -23,23 +93,6 @@ class ChatRequest(BaseModel):
     model: str
     messages: List[Message]
 
-def verify(token: str):
-    logger.debug(f"token {token}")
-    if token != TOKEN:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-    return None  # This returns the user data from the token
-
-from fastapi.security import OAuth2PasswordBearer
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    verify(token)
-    return   
-
-
-#OLLAMA_API_URL = "http://localhost:11434/api/generate"  # Ollama server URL
 @app.middleware("http")
 async def log_request_headers(request: Request, call_next):
     headers = {k: v for k, v in request.headers.items()}
@@ -73,7 +126,7 @@ class ChatRequest(BaseModel):
 
 
 @app.get("/api/tags", response_model=Dict[str, List[Model]])
-def list_models(token: str = Depends(get_current_user)):
+def list_models(token: str = Depends(validate_token)):
     return {
         "models": [
             {
@@ -94,13 +147,13 @@ def list_models(token: str = Depends(get_current_user)):
     }
 
 @app.get("/api/version")
-def get_version(token: str = Depends(get_current_user)):
+def get_version(token: str = Depends(validate_token)):
     return {"version": "0.5.7"}
 
 
 @app.post("/api/chat")
-def handle_models(request: ChatRequest,token: str = Depends(get_current_user)):
-    # Extract the user's message content
+def handle_models(request: ChatRequest,token: str = Depends(get_user)):
+    # Extract the user's message content±
     #user_content = request.messages[0].content
 
     # Process the request using the supermanPrepare.kickoff function
@@ -109,7 +162,7 @@ def handle_models(request: ChatRequest,token: str = Depends(get_current_user)):
     # Construct the response
     return {
         "model": "superman",
-        "created_at": datetime.utcnow().isoformat() + "Z",
+        "created_at": "2023-12-07T09:32:18.757212583-08:00",
         "message": {
             "role": "assistant",
             "content": "result_prepare"
@@ -123,12 +176,8 @@ def handle_models(request: ChatRequest,token: str = Depends(get_current_user)):
         "eval_duration": 4799921000
     }
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
 
-PORT = int(os.getenv("PORT",8031))  # Define a default port
+PORT = int(os.getenv("PORT",8080))  # Define a default port
 
 async def run():
     config = uvicorn.Config(app, host="127.0.0.1", port=PORT, loop="asyncio")
